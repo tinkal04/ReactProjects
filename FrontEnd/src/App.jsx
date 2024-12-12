6038a4e1ce8b4db15b43bbaf35374148f9d61b14
tinkal04 <pateltinkal0405@gmail.com>
2024-12-12 21:23:00 +0530

    Your commit message

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Admin from "./pages/Admin/Admin";
+import Error from "./pages/Error/Error";
+import "./styles/global.css"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ FrontEnd/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Home.module.css"; // Importing CSS module
+
+const Home = () => {
+  return (
+    <div>
+        
+    </div>
+  );
+};
+
+export default Home;
